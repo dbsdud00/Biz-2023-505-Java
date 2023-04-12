2928b07df15698ef238f9c872b00bd1ef6aa222c
dbsdud00 <dbsdudsw@icloud.com>
2023-04-12 15:37:04 +0900

    second

---
+++ hello/src/com/callor/hello/HelloGwangju.java
@@ -0,0 +1,20 @@
+package com.callor.hello;
+
+// 키워드 뒤 괄호가 열리고 닫히면 메쏘드 (초록색)
+// 첫글자가 대문자면 클래스 (파란색)
+// 오브젝트(객체) (하늘색, ex.out)
+
+public class HelloGwangju {
+	/*
+	 * main() method는 프로젝트 전체에서 한번만 등장하는 것이 원칙이다.
+	 * eclipse 로 코드을 작성할 때는 
+	 * 현재 보고있는 클래스 파일의 main() method를 진입점 method로 본다.
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Korea");
+		System.out.println(30+40);
+		System.out.println(30*40);
+		
+	}
+}
--- hello/src/com/callor/hello/HelloKorea.java
+++ hello/src/com/callor/hello/HelloKorea.java
@@ -1,5 +1,27 @@
 package com.callor.hello;
 
-public class HelloKorea {
-
-}
+// 클래스를 만드는 방법
+/*
+ * Java의 클래스 파일 만들기
+ * Java의 클래스 파일은 Source Code File 이라고 한다.
+ * 클래스명.java 라는 이름으로 파일이 생성된다.
+ * 여기에서는 HelloKorea.java 라는 이름으로 파일이 생성된다.
+ * 
+ * "클래스 명"은 가급적 2개 이상의 단어를 조합하여 사용한다.
+ * "클래스 명" 첫글자는 반드시 대문자를 사용한다.
+ * 이후 부터는 영문 대소문자, 숫자, 언더스코어(_)를 사용할 수 있다.
+ * 첫글자를 언더스코어로 사용하기도 하지만 최근 패턴은 권장하지 않는다.
+ * 
+ */
+public class HelloKorea { // block은 blade(중괄호)로 시작
+	/*
+	 * main method
+	 * main method 는 명령문 그룹의 규칙이 정해져 있다.
+	 * 시험에 나올 예정
+	 * 진입점, 시작점 method 라고 한다.
+	 * java로 만든 코드가 시작되는 곳이다.
+	 */
+	public static void main(String[] args) {
+		
+	}
+}// end HelloKorea block, blade 는 반드시 쌍을 이루어야 한다.
