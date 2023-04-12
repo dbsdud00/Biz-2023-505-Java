0a7319c1d96c0c07d2dfdd7358516e2f059b6660
dbsdud00 <dbsdudsw@icloud.com>
2023-04-12 16:31:52 +0900

    숫자데이터

--- hello/src/com/callor/hello/NumberOp.java
+++ hello/src/com/callor/hello/NumberOp.java
@@ -8,6 +8,27 @@ public class NumberOp {
 		System.out.println(30 + 40);
 		System.out.println(30 - 40);
 		System.out.println(30 * 40);
-		System.out.println("30 / 40 = " + 30 / 40);
+		// 정수의 나눗셈은 몫을 구하는 연산
+		System.out.println(30 / 40);
+		// 정수의 나머지 연산
+		System.out.println(30 % 40);
+		
+		// 실수와 실수를 나눗셈 연산
+		// 연산 결과도 실수로 나타난다.
+		System.out.println(30.0 / 40.0);
+		// 소수점 이햐 17째 자리에서 버림 현상이 나타난다.
+		System.out.println(4.0/3.0); // 배정도 실수 : double
+		
+		System.out.println(4.0F / 3.0F); // 단정도 실수 : float
+		
+		/*
+		 * 코드에서 사용하는 숫자는 정수와 실수가 있고
+		 * 큰 정수(int, Integer)와 작은 정수(Long), 배정도 실수(double), 단정도 실수(float)가 있다.
+		 * 배정도 실수 : 소수점 이하 16자리까지 표시
+		 * 단정도 실수 : 소수점 이하 7자리까지 표시
+		 */
+		System.out.println(999_999_999); // int
+		System.out.println(999_999_999_999_999L); //Long
 	}
 }
+
